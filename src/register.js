404e7e7895dac0488aec5cb1782605fb132c7b6f
MirEnovi <garciaivonne11@gmail.com>
2018-08-11 18:13:39 -0500

    add last changes

--- src/register.js
+++ src/register.js
@@ -1,7 +1,13 @@
 // // Login redirección
 
-let button = document.getElementById('login');
+let button = document.getElementById('google-log');
 
 button.addEventListener('click', (event) => {
+  location.href = 'home.html';
+});
+
+let button2 = document.getElementById('twitter-log');
+
+button2.addEventListener('click', (event) => {
   location.href = 'home.html';
 });
